94100da10fa2fd35baff2373536735e786273b4b
Swaggerino <bjoci93@gmail.com>
2017-02-07 14:41:09 +0100

    logic works

--- tk-01-tw-morse/calculatesystemstate.c
+++ tk-01-tw-morse/calculatesystemstate.c
@@ -1,4 +1,5 @@
 #include "selfprot.h"
+#include <math.h>
 
 /*
  * Function: CalculateSystemState
@@ -9,4 +10,36 @@
  * env: contains environment data and calculated state will be stored in it
  */
 void CalculateSystemState(EnvironmentData *env) {
+	int x = env->acc_x;
+	int y = env->acc_y;
+	int z = env->acc_z;
+	int temp = env->temp;
+	int temp_high = 0;
+	int acc_high = 0;
+	int squaree = x*x+z*z+y*y;
+	if (x >= 20 || x <= -20 || y >= 20 || y <= -20 || z >= 20 || z <= -20 || squaree >= 900)
+		{
+			acc_high = 1;
+		}
+	if (temp > 34)
+		{
+			temp_high = 1;
+		}
+
+	if (temp_high == 1 && acc_high == 1)
+	{
+		env->state = HIGH_ACCELERATION_AND_TEMP;
+	}
+	else if ( temp_high == 1 && acc_high == 0)
+	{
+		env->state = HIGH_TEMP;
+	}
+	else if ( temp_high == 0 && acc_high == 1)
+	{
+		env->state = HIGH_ACCELERATION;
+	}
+	else
+	{
+		env->state = OK;
+	}
 }
--- tk-01-tw-morse/createenvironmentdata.c
+++ tk-01-tw-morse/createenvironmentdata.c
@@ -11,12 +11,10 @@
  * temp: magnitude of the temperature
  */
 EnvironmentData CreateEnvironmentData(float acc_x, float acc_y, float acc_z, signed int temp) {
-	/* We store the acceleration values multiplied by 10 at every (x,y,z)
-	 * component of the acceleration vector. */
-	int acc_x_int = (int)((acc_x + 0,05)*10);
-	int acc_y_int = (int)((acc_y + 0,05)*10);
-	int acc_z_int = (int)((acc_z + 0,05)*10);
+	int acc_x_int = (int)(acc_x*10);
+	int acc_y_int = (int)(acc_y*10);
+	int acc_z_int = (int)(acc_z*10);
 
-	EnvironmentData environment_data = {acc_x_int, acc_y_int, acc_z_int, temp};
-	return environment_data;
+	EnvironmentData env = {acc_x_int, acc_y_int, acc_z_int, temp};
+	return env;
 }
