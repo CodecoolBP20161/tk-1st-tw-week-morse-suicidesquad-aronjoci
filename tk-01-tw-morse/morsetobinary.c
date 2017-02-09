cdf116e624d5b7889804bc80cbd6405e7c02b264
Áron Kékes <kekes.aron@gmail.com>
2017-02-09 11:42:06 +0100

    The logic writes the last 8 valid bit of the binary code into the output

--- tk-01-tw-morse/morsetobinary.c
+++ tk-01-tw-morse/morsetobinary.c
@@ -1,4 +1,5 @@
 #include "selfprot.h"
+#include <stdio.h>
 
 /*
  * Function: MorseToBinary
@@ -11,4 +12,42 @@
  * returns: the number of used bits for morse
  */
 int MorseToBinary(char input[], char output[]) {
+	int number_of_valid_bits=0;
+
+	for(int i; input[i] != '\0'; i++){
+		switch(input[i]){
+			case '.':
+				*output = *output<<1;
+				*output = *output | 1;
+				number_of_valid_bits += 1;
+				break;
+
+			case '-':
+				*output = *output<<3;
+				*output = *output | 7;
+				number_of_valid_bits += 3;
+				break;
+
+			case ' ':
+				*output = *output<<1;
+				number_of_valid_bits += 1;
+				break;
+
+			case '/':
+				*output = *output<<3;
+				number_of_valid_bits += 3;
+				break;
+
+			case '\t':
+				*output<<7;
+				number_of_valid_bits += 7;
+				break;
+
+			default:
+				printf("Not valid character given");
+				return -1;
+		}
+	}
+	*output<<(256-number_of_valid_bits);
+	return number_of_valid_bits;
 }
